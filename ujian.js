7f56692b97543e848b28e0cbdea91a0bc2c61ae6
liakook <46911724+liakook@users.noreply.github.com>
2019-02-18 12:00:23 +0700

    Add files via upload

--- ujian.js
+++ ujian.js
@@ -6,8 +6,22 @@
 // console.log(berapatahun(1000,5,50,1200)) // 2tahun
 // console.log(berapatahun(1500000,2.5,10000,2000000))
 
-function berapatahun(n,p,v,t){
-    var tahun = t/(n + (v + (n * (p/100) )))
-    return tahun
+// function berapatahun(n,p,v,t){
+//     var tahun = t/(n + (v + (n * (p/100) )))
+//     return tahun
+// }
+// console.log(berapatahun(1500000,2.5,10000,2000000))
+
+// jawaban ujian No. 1: Monday, 18 Feb 2019
+var berapatahun = (awal,percent,visitor,target) => {
+    var tahun = 0
+    percent = percent / 100
+    do{
+        awal = awal + (awal*percent) + visitor
+        tahun ++
+    }
+    while(awal < target)
+    return ' Kota akan mencapai ' + target + ' dalam ' + tahun + ' tahun '
 }
 console.log(berapatahun(1500000,2.5,10000,2000000))
+console.log(berapatahun(10,50,5,50))
--- ujitiga.js
+++ ujitiga.js
@@ -2,19 +2,54 @@
 sedangkan untuk menyimpan angka, bisa
 memakai array 2 dimensi (matrix). array di dalam array. */
 
-function segitigaPasc(num){
-    let pasc = ''
-for(let i = 0 ; i < 5; i++){
-    for(let j = i; j < 4; j++){
-        pasc += ' '
+// function segitigaPasc(num){
+//     let pasc = ''
+// for(let i = 0 ; i < 5; i++){
+//     for(let j = i; j < 4; j++){
+//         pasc += ' '
+//     }
+//     for(let k = 0; k <= i; k++){
+//         pasc += '*'
+//     }
+//     for(let l = 0; l <= i-1; l++){
+//         pasc += '*'
+//     }
+//     pasc += '\n'
+// }
+// }
+// console.log(segitigaPasc(5))
+
+// Jawaban mas fikri:
+// result = [
+//     [1],
+//     [1,1],
+//     [1,2,1],
+//     [1,3,3,1],
+//     [1,4,6,4,1]
+
+// result = []
+
+function segitigaPasc(n=5){
+    var result = [];
+    result[0] = [1];
+    result[1] = [1,1];
+    for(var row = 2; row < n; row++){
+        result[row] = [1];
+        for(var col = 1; col <= row - 1; col++){
+            result[row][col] = result[row-1][col] + result[row-1][col-1]
+        }
+        result[row].push(1);
     }
-    for(let k = 0; k <= i; k++){
-        pasc += '*'
-    }
-    for(let l = 0; l <= i-1; l++){
-        pasc += '*'
-    }
-    pasc += '\n'
+    return result
 }
+
+var print = segitigaPasc(5)
+var output = ''
+for(var i = 0; i<print.length; i++){
+    for(var spasi = print.length; spasi > i; spasi--){
+        output += ' '
+    }
+    output += print[i].join(' ')
+    output += '\n'
 }
-console.log(segitigaPasc(5))
+console.log(output)
